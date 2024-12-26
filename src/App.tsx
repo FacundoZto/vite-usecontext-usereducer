10adfbc4cff173bae8005fd28500908d6b552122
facundozto <facundozerbato@outlook.com>
2024-12-26 13:20:18 -0300

    cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react';
+import Products from './components/Products/Products';
+import HashLoader from "react-spinners/HashLoader";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('/api/products?limit=5')
+    .then(res=>res.json())
+    .then(json=> {
+      setProducts(json)
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
+      <HashLoader 
+        size={60} 
+        color='#77f43c' 
+        loading={loading} 
+      />
+      <Products products={products} onAddToCart={(product) => console.log(product)} />
     </>
   )
 }
--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,23 +1,23 @@
-import './Product.css';
-import { AddToCartIcon } from '../Icons';
-import { Product } from '../../Interfaces/Product';
+import { AddToCartIcon } from "../Icons";
+import { Product } from "../../Interfaces/Product";
+import style from "./Products.module.css";
 
 interface ProductsProps {
   products: Product[];
   onAddToCart: (product: Product) => void;
 }
 
-const Products:React.FC<ProductsProps> = ({ products, onAddToCart }) => {
+const Products: React.FC<ProductsProps> = ({ products, onAddToCart }) => {
   return (
-    <main className='products'>
+    <main className={style.products}>
       <ul>
         {products.map((product: Product) => (
-          <li key={product.id} className='product'>
-            <img src={product.image} alt={product.title} />
-            <div className='product-info'>
+          <li key={product.id} className={style.product}>
+            <img src={product.image} alt={product.title}/>
+            <div className={style.product_info}>
               <h3>{product.title}</h3>
               <p>{product.description}</p>
-              <p>${product.price}</p>
+              <p>$ {product.price}</p>
               <button onClick={() => onAddToCart(product)}>
                 <AddToCartIcon />
                 Add to cart
